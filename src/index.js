e20b056ca6da1bad0469a1074961fe3d72e0e2bb
alexandrettaBit <alxsalaza@gmail.com>
2022-07-12 18:54:47 -0500

    agregadas imágenes al html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import validator from './validator.js';
 
+function pagar(){
+    let clientName = document.getElementById("clientName").value;
+    let credit = document.getElementById("credit").value;
+    let cvv = document.getElementById("cvv").value;
+
+    let boton = document.getElementById("purchaise");
+    boton.addEventListener("click", pagar(), false);
+    alert("Pago procesado");
+}
+
+
+
+
+
 console.log(validator);
