577e288c57e75424f0435c506bd18ff51ba9b09b
alexandrettaBit <alxsalaza@gmail.com>
2022-07-18 21:18:49 -0500

    Captura de datos completa

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@
 function getData(){
     const name = document.getElementById('clientName').value;
     console.log(name);
+
+    const card = document.getElementById('cardType').value;
+    console.log(card);
+
+    const number = document.getElementById('cardNumber').value;
+    console.log(number);//Requiere el masking
+
+    const date = document.getElementById('expiration').value;
+    console.log(date);
+
+    const cvv = document.getElementById('cvv').value;
+    console.log(cvv);
 }
 
 const btn = document.getElementById('btnSubmit').addEventListener("click",getData);
