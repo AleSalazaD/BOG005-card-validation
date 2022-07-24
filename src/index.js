fccf99f0cd9fb403d0ad230f6dbb957430ce5d24
alexandrettaBit <alxsalaza@gmail.com>
2022-07-24 14:00:19 -0500

    Organicé algunos estilos y acomodé algo del código index.js para la función

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
-//import validator from './validator.js';
+//Se importa el objeto validator desde el archivo validator.js *Debe tener una contraparte que exporte en el otro archivo*
+import validator from './validator.js';
+console.log(validator);
 
 function getData(){
-    const name = document.getElementById('clientName').value;
+    const name = document.getElementById('clientName').value; //Captura el nombre
     console.log(name);
 
-    const card = document.getElementById('cardType').value;
+    const card = document.getElementById('cardType').value; //Captura el tipo de tarjeta
     console.log(card);
+// ***********
+    let strNumber = document.getElementById('cardNumber').value;
+     //Captura el #tarjeta en string
+    strNumber = Array.from(strNumber); //Convierte la data en Array
+    console.log(strNumber);
+    
+    let arrayNum = [];
+    for(let i=0; i<strNumber.length; i++){
+        let elemento = parseInt(strNumber[i])
+        arrayNum.push(elemento)
+    }
+    arrayNum = arrayNum.reverse();
+    console.log(arrayNum);//Aún Requiere el masking, requiere las operaciones para Luhn (en validator.js)
+    
+    console.log(validator.isValid);//Devuelve el resultado de funcion isValid (está mandando resultado undefined)
 
-    let number = document.getElementById('cardNumber').value;
-    const arrayNumber = number.split("").reverse();
-    console.log(arrayNumber);//Requiere el masking, yconvertir a array
+    const month = document.getElementById('month').value;
+    console.log(month);
 
-
-    const date = document.getElementById('expiration').value;
-    console.log(date);
+    const year = document.getElementById('year').value;
+    console.log(year);
 
     const cvv = document.getElementById('cvv').value;
     console.log(cvv);
 }
 
-const btn = document.getElementById('btnSubmit').addEventListener("click",getData);
+document.getElementById('btnSubmit').addEventListener("click",getData);
     
-
-
-//console.log(validator);
+//export  {arrayNumber};
--- src/validator.js
+++ src/validator.js
@@ -1,8 +1,17 @@
-//const validator = {
+//import arrayNumber from "./index.js";
+//Objeto validator que contiene las funciones y que luego va a ser importado al index.js para poder interactuar.
+const arrayNumber = [];
+const validator = {
   
-  function isValid(){
+  isValid: function isValid(){
     
-  }
-//};
-
-export default validator;
+      let arreglo = [];
+      for (let i = 0; i < arrayNumber.length; i += 2) {
+        arreglo.push(arrayNumber[i]);
+      }
+      return arreglo;
+      
+    }
+};
+//La export es la contraparte que se importó en el index.js *Hay una contraparte de import en el index.js
+export default {validator};
